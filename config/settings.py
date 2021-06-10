d83f05eef9280a65e1f5a4dc4918cf7195887e94
Hyeonjun <phj3046@gmail.com>
2021-06-10 22:42:20 +0900

    dstagram

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-vgyxrkm%mnc_*9^+fj$dbbqf$h_ejg@ck&j&76zl-hchw-laon
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 
 
 # Application definition
